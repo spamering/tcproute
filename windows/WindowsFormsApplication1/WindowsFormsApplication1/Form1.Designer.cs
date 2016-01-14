f46898f4749b1a4e6e5f04317b7f9d6d1875c609
GameXG <GameXG@Gmail.Com>
2016-01-14 23:19:28 +0800

    增加 windows 图形界面

---
+++ windows/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace TcpRoute2Windows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tcpRoute = new System.Diagnostics.Process();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.启动ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.重新启动ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.停止ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button3 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.MaxLength = 100;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(675, 494);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(12, 533);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "启动";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(174, 533);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "停止";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tcpRoute
+            // 
+            this.tcpRoute.EnableRaisingEvents = true;
+            this.tcpRoute.StartInfo.CreateNoWindow = true;
+            this.tcpRoute.StartInfo.Domain = "";
+            this.tcpRoute.StartInfo.LoadUserProfile = false;
+            this.tcpRoute.StartInfo.Password = null;
+            this.tcpRoute.StartInfo.StandardErrorEncoding = null;
+            this.tcpRoute.StartInfo.StandardOutputEncoding = null;
+            this.tcpRoute.StartInfo.UserName = "";
+            this.tcpRoute.SynchronizingObject = this;
+            this.tcpRoute.OutputDataReceived += new System.Diagnostics.DataReceivedEventHandler(this.process1_ErrorDataReceived);
+            this.tcpRoute.ErrorDataReceived += new System.Diagnostics.DataReceivedEventHandler(this.process1_ErrorDataReceived);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(270, 537);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(222, 16);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "出错退出时自动重新启动（最多6次）";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "000000";
+            this.notifyIcon1.BalloonTipTitle = "000";
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.启动ToolStripMenuItem,
+            this.重新启动ToolStripMenuItem,
+            this.停止ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(125, 92);
+            // 
+            // 启动ToolStripMenuItem
+            // 
+            this.启动ToolStripMenuItem.Name = "启动ToolStripMenuItem";
+            this.启动ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.启动ToolStripMenuItem.Text = "启动";
+            this.启动ToolStripMenuItem.Click += new System.EventHandler(this.启动ToolStripMenuItem_Click);
+            // 
+            // 重新启动ToolStripMenuItem
+            // 
+            this.重新启动ToolStripMenuItem.Name = "重新启动ToolStripMenuItem";
+            this.重新启动ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.重新启动ToolStripMenuItem.Text = "重新启动";
+            this.重新启动ToolStripMenuItem.Click += new System.EventHandler(this.重新启动ToolStripMenuItem_Click);
+            // 
+            // 停止ToolStripMenuItem
+            // 
+            this.停止ToolStripMenuItem.Name = "停止ToolStripMenuItem";
+            this.停止ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.停止ToolStripMenuItem.Text = "停止";
+            this.停止ToolStripMenuItem.Click += new System.EventHandler(this.停止ToolStripMenuItem_Click);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.Location = new System.Drawing.Point(93, 533);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "重新启动";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 590);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "TcpRoute2";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Diagnostics.Process tcpRoute;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 启动ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 重新启动ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 停止ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ windows/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TcpRoute2Windows
+{
+    public partial class Form1 : Form
+    {
+        bool autoRestart = false; // 主要是防止用户手动关闭也会自动重启
+        int errExit = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide(); //或者是this.Visible = false;
+            }
+            start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            errExit = 0;
+            start();
+        }
+
+        delegate void DelChangText(string buf);
+
+        void TcpRouteExit(object sender, EventArgs e)
+        {
+            启动ToolStripMenuItem.Enabled = true;
+            停止ToolStripMenuItem.Enabled = false;
+            button1.Enabled = true;
+            button2.Enabled = false;
+            intoText("tcproute 退出" + Environment.NewLine);
+
+            if (autoRestart == true && checkBox1.Checked == true && errExit < 5)
+            {
+                errExit += 1;
+                start();
+            }
+        }
+
+        void start()
+        {
+            if (checkBox1.Checked == true)
+            {
+                autoRestart = true;
+            }
+            else
+            {
+                autoRestart = false;
+            }
+            启动ToolStripMenuItem.Enabled = false;
+            停止ToolStripMenuItem.Enabled = true;
+            button1.Enabled = false;
+            button2.Enabled = true;
+            ThreadStart ts = new ThreadStart(tcpRouteStart);
+            Thread th = new Thread(ts);
+            th.Start();
+        }
+
+        void tcpRouteStart()
+        {
+            tcpRoute = new Process();
+            tcpRoute.StartInfo.FileName = "D:\\golang\\src\\github.com\\gamexg\\TcpRoute2\\TcpRoute2.exe";
+            tcpRoute.StartInfo.WorkingDirectory = "D:\\golang\\src\\github.com\\gamexg\\TcpRoute2\\";
+            tcpRoute.StartInfo.Arguments = "";
+            tcpRoute.StartInfo.CreateNoWindow = true;
+           // tcpRoute.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            tcpRoute.StartInfo.UseShellExecute = false;
+            tcpRoute.StartInfo.RedirectStandardOutput = true;
+            tcpRoute.StartInfo.StandardOutputEncoding = Encoding.UTF8;
+            tcpRoute.StartInfo.StandardErrorEncoding = Encoding.UTF8;
+            tcpRoute.StartInfo.RedirectStandardError = true;
+
+            // 注册关闭事件
+            tcpRoute.EnableRaisingEvents = true;
+            tcpRoute.SynchronizingObject = this;
+            tcpRoute.Exited += new EventHandler(TcpRouteExit);
+
+
+            tcpRoute.OutputDataReceived += new DataReceivedEventHandler(process1_ErrorDataReceived);
+            tcpRoute.ErrorDataReceived += new DataReceivedEventHandler(process1_ErrorDataReceived);
+
+            tcpRoute.Start();
+
+            tcpRoute.BeginOutputReadLine();
+            tcpRoute.BeginErrorReadLine();
+
+            tcpRoute.WaitForExit();
+        }
+
+
+
+
+        void intoText(string buf)
+        {
+            try
+            {
+                if (this.InvokeRequired)
+                {
+                    this.BeginInvoke(new DelChangText(intoText), buf);
+                }
+                else
+                {
+                    if (textBox1 != null)
+                    {
+                        textBox1.AppendText(buf);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        void tcpRouteClose()
+        {
+            try
+            {
+                if (tcpRoute != null && tcpRoute.HasExited == false)
+                {
+                    tcpRoute.CancelOutputRead();
+                    tcpRoute.CancelErrorRead();
+
+                    tcpRoute.CloseMainWindow();
+                    tcpRoute.Kill();
+                    // tcpRoute.Close();
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            autoRestart = false;
+            tcpRouteClose();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Lines.Length > 50)
+            {
+                textBox1.Text = textBox1.Text.Remove(0, textBox1.Lines[0].Length + Environment.NewLine.Length);
+            }
+            textBox1.SelectionStart = textBox1.Text.Length;
+            textBox1.ScrollToCaret();            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            tcpRouteClose();
+        }
+
+        private void process1_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(e.Data))
+            {
+                intoText(e.Data + Environment.NewLine);
+            }
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            if (this.WindowState== FormWindowState.Minimized)
+            {
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Minimized;
+            }
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide(); //或者是this.Visible = false;
+            }
+        }
+
+        private void 启动ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            errExit = 0;
+            start();
+        }
+
+        private void 停止ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            autoRestart = false;
+            tcpRouteClose();
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void 重新启动ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            autoRestart = false;
+            tcpRouteClose();
+            errExit = 0;
+            start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            autoRestart = false;
+            tcpRouteClose();
+            errExit = 0;
+            start();
+        }
+    }
+}
